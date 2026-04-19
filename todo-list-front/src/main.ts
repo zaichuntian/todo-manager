2f0ccce5266141821df68703d463b46b9a6b901f
zaichuntian <3034983887@qq.com>
2026-04-20 06:29:00 +0800

    fix: 调整库文件引入的顺序

--- todo-list-front/src/main.ts
+++ todo-list-front/src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue';
-import App from './App.vue';
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
-import router from './router';
 import { createPinia } from 'pinia';
+import ElementPlus from 'element-plus';
+import router from './router';
 import './assets/css/main.css';
+import 'element-plus/dist/index.css';
+import App from './App.vue';
 // @ts-ignore
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'; // 导入中文包
 

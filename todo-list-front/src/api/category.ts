cebe43af676f5d26f43249e99fbaee7ab3b5374e
zaichuntian <3034983887@qq.com>
2026-04-21 06:58:19 +0800

    feat: 添加分类管理api

---
+++ todo-list-front/src/api/category.ts
@@ -0,0 +1,26 @@
+import request from '../utils/request';
+
+// 获取分类列表
+export function getCategoriesApi() {
+  return request.get('/categories');
+}
+
+// 添加分类
+export function addCategoryApi(data: { name: string }) {
+  return request.post('/categories', data);
+}
+
+// 修改分类
+export function updateCategoryApi(uuid: string, data: { name: string }) {
+  return request.put(`/categories/${uuid}`, data);
+}
+
+// 删除分类
+export function deleteCategoryApi(uuid: string) {
+  return request.delete(`/categories/${uuid}`);
+}
+
+// 获取分类及其任务
+export function getCategoryWithTodosApi(uuid: string) {
+  return request.get(`/categories/${uuid}/todos`);
+}

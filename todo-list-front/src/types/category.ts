49fb1fbb05c065e2b89ed8174e0f07938d9d44bf
zaichuntian <3034983887@qq.com>
2026-04-21 06:58:29 +0800

    feat: 添加分类管理所需类型

---
+++ todo-list-front/src/types/category.ts
@@ -0,0 +1,26 @@
+// 分类相关类型定义
+import type { FormRules, UserBasic } from './common';
+import type { Todo } from './todo';
+
+export interface Category {
+  uuid: string;
+  name: string;
+  userUuid: string;
+  user?: UserBasic;
+  todos?: Todo[];
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface CategoryFormData {
+  uuid: string;
+  name: string;
+}
+
+export type CategoryRules = FormRules;
+
+export type CategoryListResponse = {
+  list: Category[];
+};
+
+export type CategoryWithTodosResponse = Category;

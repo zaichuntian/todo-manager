fed1deaaef1905c5930a3edf2d040bc8491da5bc
zaichuntian <3034983887@qq.com>
2026-04-21 06:25:34 +0800

    feat: 添加分类的service

---
+++ todo-list-backend/src/services/category.service.ts
@@ -0,0 +1,52 @@
+import Category from '../models/category.model';
+import User from '../models/user.model';
+import Todo from '../models/todo.model';
+
+export class CategoryService {
+  // 查询用户的所有分类
+  static async findUserCategories(userUuid: string) {
+    return await Category.findAll({
+      where: { userUuid, isDeleted: 1 },
+      order: [['createdAt', 'DESC']],
+    });
+  }
+
+  // 根据UUID查询单个分类
+  static async findByUuid(uuid: string) {
+    return await Category.findOne({
+      where: { uuid, isDeleted: 1 },
+    });
+  }
+
+  // 创建分类
+  static async create(category: any) {
+    return await Category.create(category);
+  }
+
+  // 更新分类
+  static async updateByUuid(uuid: string, data: any) {
+    return await Category.update(data, {
+      where: { uuid, isDeleted: 1 },
+    });
+  }
+
+  // 删除分类
+  static async deleteByUuid(uuid: string) {
+    return await Category.update({ isDeleted: 0 }, { where: { uuid, isDeleted: 1 } });
+  }
+
+  // 查询分类及其任务
+  static async findCategoryWithTodos(categoryUuid: string) {
+    return await Category.findOne({
+      where: { uuid: categoryUuid, isDeleted: 1 },
+      include: [
+        {
+          model: Todo,
+          as: 'todos',
+          where: { isDeleted: 1 },
+          order: [['createdAt', 'DESC']],
+        },
+      ],
+    });
+  }
+}

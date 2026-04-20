677355b743fbaa1257fc65f2630b1c45f8c2f72d
zaichuntian <3034983887@qq.com>
2026-04-21 06:23:53 +0800

    feat: 添加分类关联

--- todo-list-backend/src/models/index.ts
+++ todo-list-backend/src/models/index.ts
@@ -1,8 +1,17 @@
 import Todo from './todo.model';
 import User from './user.model';
+import Category from './category.model';
 
-// 一个用户多个任务
-User.hasMany(Todo, { foreignKey: 'userId' });
-Todo.belongsTo(User, { foreignKey: 'userId' });
+// 用户与任务的关联
+User.hasMany(Todo, { foreignKey: 'userUuid', sourceKey: 'uuid', as: 'todos' });
+Todo.belongsTo(User, { foreignKey: 'userUuid', targetKey: 'uuid', as: 'user' });
 
-export { User, Todo };
+// 用户与分类的关联
+User.hasMany(Category, { foreignKey: 'userUuid', sourceKey: 'uuid', as: 'categories' });
+Category.belongsTo(User, { foreignKey: 'userUuid', targetKey: 'uuid', as: 'user' });
+
+// 分类与任务的关联
+Category.hasMany(Todo, { foreignKey: 'categoryUuid', sourceKey: 'uuid', as: 'todos' });
+Todo.belongsTo(Category, { foreignKey: 'categoryUuid', targetKey: 'uuid', as: 'category' });
+
+export { User, Todo, Category };

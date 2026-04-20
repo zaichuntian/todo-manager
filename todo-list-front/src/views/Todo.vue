7a6ffc862bc872ca35a21116b2a03f3f2f12a2d4
zaichuntian <3034983887@qq.com>
2026-04-20 21:54:38 +0800

    style: 使用卡片包裹页面内容，使页面更好看

--- todo-list-front/src/views/Todo.vue
+++ todo-list-front/src/views/Todo.vue
@@ -1,88 +1,90 @@
 <template>
-  <el-card>
-    <div class="header-bar">
-      <h3>任务管理</h3>
-      <el-button type="primary" @click="handleAdd">新增任务</el-button>
-    </div>
+  <div class="todo-page">
+    <el-card>
+      <div class="header-bar">
+        <h3>任务管理</h3>
+        <el-button type="primary" @click="handleAdd">新增任务</el-button>
+      </div>
 
-    <el-table :data="tableData" border style="width: 100%">
-      <el-table-column type="index" label="序号" width="80" align="center" />
+      <el-table :data="tableData" border style="width: 100%">
+        <el-table-column type="index" label="序号" width="80" align="center" />
 
-      <el-table-column label="创建者" align="center">
-        <template #default="{ row }">
-          {{ row.user?.username || '未知用户' }}
-        </template>
-      </el-table-column>
+        <el-table-column label="创建者" align="center">
+          <template #default="{ row }">
+            {{ row.user?.username || '未知用户' }}
+          </template>
+        </el-table-column>
 
-      <el-table-column prop="title" label="标题" align="center" />
-      <el-table-column prop="content" label="内容" align="center" />
+        <el-table-column prop="title" label="标题" align="center" />
+        <el-table-column prop="content" label="内容" align="center" />
 
-      <el-table-column label="创建时间" align="center">
-        <template #default="{ row }">
-          {{ formatTime(row.createdAt) }}
-        </template>
-      </el-table-column>
+        <el-table-column label="创建时间" align="center">
+          <template #default="{ row }">
+            {{ formatTime(row.createdAt) }}
+          </template>
+        </el-table-column>
 
-      <el-table-column label="更新时间" align="center">
-        <template #default="{ row }">
-          {{ formatTime(row.updatedAt) }}
-        </template>
-      </el-table-column>
+        <el-table-column label="更新时间" align="center">
+          <template #default="{ row }">
+            {{ formatTime(row.updatedAt) }}
+          </template>
+        </el-table-column>
 
-      <!-- 新增：完成状态列 -->
-      <el-table-column label="完成状态" width="180" align="center">
-        <template #default="{ row }">
-          <!-- 仅自己的任务可切换状态 -->
-          <div class="status-switch-wrapper">
-            <span class="status-label disabled" :class="{ active: row.status === 0 }"> 未完成 </span>
-            <el-switch
-              v-model="row.status"
-              :active-value="1"
-              :inactive-value="0"
-              :disabled="!isMyTask(row)"
-              class="custom-switch"
-              @change="(val: number) => handleStatusChange(row, val)"
-            />
-            <span class="status-label enabled" :class="{ active: row.status === 1 }"> 已完成 </span>
-          </div>
-        </template>
-      </el-table-column>
+        <!-- 新增：完成状态列 -->
+        <el-table-column label="完成状态" width="180" align="center">
+          <template #default="{ row }">
+            <!-- 仅自己的任务可切换状态 -->
+            <div class="status-switch-wrapper">
+              <span class="status-label disabled" :class="{ active: row.status === 0 }"> 未完成 </span>
+              <el-switch
+                v-model="row.status"
+                :active-value="1"
+                :inactive-value="0"
+                :disabled="!isMyTask(row)"
+                class="custom-switch"
+                @change="(val: number) => handleStatusChange(row, val)"
+              />
+              <span class="status-label enabled" :class="{ active: row.status === 1 }"> 已完成 </span>
+            </div>
+          </template>
+        </el-table-column>
 
-      <el-table-column label="操作" width="200" align="center">
-        <template #default="{ row }">
-          <el-button v-if="isMyTask(row)" type="primary" link @click="handleEdit(row)"> 编辑 </el-button>
-          <el-button v-if="isMyTask(row)" type="danger" link @click="handleDelete(row)"> 删除 </el-button>
-          <span v-else style="color: #999">无操作权限</span>
-        </template>
-      </el-table-column>
-    </el-table>
+        <el-table-column label="操作" width="200" align="center">
+          <template #default="{ row }">
+            <el-button v-if="isMyTask(row)" type="primary" link @click="handleEdit(row)"> 编辑 </el-button>
+            <el-button v-if="isMyTask(row)" type="danger" link @click="handleDelete(row)"> 删除 </el-button>
+            <span v-else style="color: #999">无操作权限</span>
+          </template>
+        </el-table-column>
+      </el-table>
 
-    <div class="pagination-wrapper">
-      <el-pagination
-        v-model:current-page="pageNum"
-        v-model:page-size="pageSize"
-        :total="total"
-        layout="total, prev, pager, next, sizes"
-        @current-change="getList"
-        @size-change="getList"
-      />
-    </div>
-  </el-card>
+      <div class="pagination-wrapper">
+        <el-pagination
+          v-model:current-page="pageNum"
+          v-model:page-size="pageSize"
+          :total="total"
+          layout="total, prev, pager, next, sizes"
+          @current-change="getList"
+          @size-change="getList"
+        />
+      </div>
+    </el-card>
 
-  <el-dialog v-model="dialogVisible" title="任务信息" @keyup.enter="handleSubmit">
-    <el-form :model="form" :rules="rules" ref="formRef" label-width="80px">
-      <el-form-item label="标题" prop="title">
-        <el-input v-model="form.title" placeholder="请输入标题" />
-      </el-form-item>
-      <el-form-item label="内容" prop="content">
-        <el-input v-model="form.content" type="textarea" :rows="3" />
-      </el-form-item>
-    </el-form>
-    <template #footer>
-      <el-button @click="dialogVisible = false">取消</el-button>
-      <el-button type="primary" @click="handleSubmit">确定</el-button>
-    </template>
-  </el-dialog>
+    <el-dialog v-model="dialogVisible" title="任务信息" @keyup.enter="handleSubmit">
+      <el-form :model="form" :rules="rules" ref="formRef" label-width="80px">
+        <el-form-item label="标题" prop="title">
+          <el-input v-model="form.title" placeholder="请输入标题" />
+        </el-form-item>
+        <el-form-item label="内容" prop="content">
+          <el-input v-model="form.content" type="textarea" :rows="3" />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="handleSubmit">确定</el-button>
+      </template>
+    </el-dialog>
+  </div>
 </template>
 
 <script setup lang="ts">
--- todo-list-front/src/views/User.vue
+++ todo-list-front/src/views/User.vue
@@ -1,78 +1,80 @@
 <template>
-  <el-card>
-    <!-- 顶部操作栏 -->
-    <div class="header-bar">
-      <h3>用户管理</h3>
-      <el-button type="primary" @click="handleAdd">新增用户</el-button>
-    </div>
+  <div class="user-page">
+    <el-card>
+      <!-- 顶部操作栏 -->
+      <div class="header-bar">
+        <h3>用户管理</h3>
+        <el-button type="primary" @click="handleAdd">新增用户</el-button>
+      </div>
 
-    <!-- 用户表格 -->
-    <el-table :data="tableData" border style="width: 100%">
-      <el-table-column type="index" label="序号" width="80" align="center" />
-      <el-table-column prop="username" width="120" label="用户名" align="center" />
-      <el-table-column label="创建时间" align="center">
-        <template #default="{ row }">
-          {{ formatTime(row.createdAt) }}
-        </template>
-      </el-table-column>
-      <el-table-column label="更新时间" align="center">
-        <template #default="{ row }">
-          {{ formatTime(row.updatedAt) }}
-        </template>
-      </el-table-column>
-      <el-table-column label="状态" width="170" align="center">
-        <template #default="{ row }">
-          <div class="status-switch-wrapper">
-            <span class="status-label disabled" :class="{ active: row.status === 0 }"> 禁用 </span>
-            <el-switch
-              v-model="row.status"
-              :active-value="1"
-              :inactive-value="0"
-              class="custom-switch"
-              @change="(val: number) => handleStatusChange(row, val)"
-            />
-            <span class="status-label enabled" :class="{ active: row.status === 1 }"> 启用 </span>
-          </div>
-        </template>
-      </el-table-column>
+      <!-- 用户表格 -->
+      <el-table :data="tableData" border style="width: 100%">
+        <el-table-column type="index" label="序号" width="80" align="center" />
+        <el-table-column prop="username" width="120" label="用户名" align="center" />
+        <el-table-column label="创建时间" align="center">
+          <template #default="{ row }">
+            {{ formatTime(row.createdAt) }}
+          </template>
+        </el-table-column>
+        <el-table-column label="更新时间" align="center">
+          <template #default="{ row }">
+            {{ formatTime(row.updatedAt) }}
+          </template>
+        </el-table-column>
+        <el-table-column label="状态" width="170" align="center">
+          <template #default="{ row }">
+            <div class="status-switch-wrapper">
+              <span class="status-label disabled" :class="{ active: row.status === 0 }"> 禁用 </span>
+              <el-switch
+                v-model="row.status"
+                :active-value="1"
+                :inactive-value="0"
+                class="custom-switch"
+                @change="(val: number) => handleStatusChange(row, val)"
+              />
+              <span class="status-label enabled" :class="{ active: row.status === 1 }"> 启用 </span>
+            </div>
+          </template>
+        </el-table-column>
 
-      <el-table-column label="操作" width="130" align="center">
-        <template #default="{ row }">
-          <el-button type="primary" link @click="handleEdit(row)">编辑</el-button>
-          <el-button type="danger" link @click="handleDelete(row)">删除</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+        <el-table-column label="操作" width="130" align="center">
+          <template #default="{ row }">
+            <el-button type="primary" link @click="handleEdit(row)">编辑</el-button>
+            <el-button type="danger" link @click="handleDelete(row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
 
-    <!-- 分页组件 -->
-    <div class="pagination-wrapper">
-      <el-pagination
-        v-model:current-page="pageNum"
-        v-model:page-size="pageSize"
-        :total="total"
-        :page-sizes="[5, 10, 15]"
-        layout="total, prev, pager, next, sizes"
-        @current-change="getUserList"
-        @size-change="getUserList"
-      />
-    </div>
-  </el-card>
+      <!-- 分页组件 -->
+      <div class="pagination-wrapper">
+        <el-pagination
+          v-model:current-page="pageNum"
+          v-model:page-size="pageSize"
+          :total="total"
+          :page-sizes="[5, 10, 15]"
+          layout="total, prev, pager, next, sizes"
+          @current-change="getUserList"
+          @size-change="getUserList"
+        />
+      </div>
+    </el-card>
 
-  <!-- 新增/编辑弹窗 -->
-  <el-dialog v-model="dialogVisible" title="用户信息">
-    <el-form :model="form" :rules="rules" ref="formRef" label-width="80px">
-      <el-form-item label="用户名" prop="username">
-        <el-input v-model="form.username" placeholder="请输入用户名" />
-      </el-form-item>
-      <el-form-item v-if="isAdd" label="密码" prop="password">
-        <el-input v-model="form.password" type="password" placeholder="请输入密码" />
-      </el-form-item>
-    </el-form>
-    <template #footer>
-      <el-button @click="dialogVisible = false">取消</el-button>
-      <el-button type="primary" @click="handleSubmit">确定</el-button>
-    </template>
-  </el-dialog>
+    <!-- 新增/编辑弹窗 -->
+    <el-dialog v-model="dialogVisible" title="用户信息">
+      <el-form :model="form" :rules="rules" ref="formRef" label-width="80px">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="form.username" placeholder="请输入用户名" />
+        </el-form-item>
+        <el-form-item v-if="isAdd" label="密码" prop="password">
+          <el-input v-model="form.password" type="password" placeholder="请输入密码" />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="handleSubmit">确定</el-button>
+      </template>
+    </el-dialog>
+  </div>
 </template>
 
 <script setup lang="ts">

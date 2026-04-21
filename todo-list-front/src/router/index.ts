ea3cd4a7301f46c2e7c48cffcac87c29ac0ce7d8
zaichuntian <3034983887@qq.com>
2026-04-22 03:59:06 +0800

    feat: 添加404、403页面路由规则，并配置路由守卫规则

--- todo-list-front/src/router/index.ts
+++ todo-list-front/src/router/index.ts
@@ -5,6 +5,9 @@ import User from '../views/User.vue';
 import Todo from '../views/Todo.vue';
 import Layout from '../layouts/Layout.vue';
 import Category from '../views/Category.vue';
+import Page404 from '../views/404.vue';
+import Page403 from '../views/403.vue';
+import { ElMessage } from 'element-plus';
 import { UserInfo } from '@/types/user.ts';
 
 const routes = [
@@ -19,6 +22,8 @@ const routes = [
       { path: '/category', component: Category },
     ],
   },
+  { path: '/403', component: Page403 },
+  { path: '/:pathMatch(.*)*', component: Page404 },
 ];
 
 const router = createRouter({
@@ -32,6 +37,11 @@ router.beforeEach(to => {
   if (to.path !== '/login' && !userInfo.token && !userInfo.userUuid) return '/login';
   // 如果用户已登录，且尝试访问登录页，重定向到首页
   if (to.path === '/login' && userInfo.token && userInfo.userUuid) return '/';
+  // 权限检查：只有超级管理员和管理员可以访问用户管理页面
+  if (to.path === '/user' && userInfo.role !== 1 && userInfo.role !== 2) {
+    ElMessage.warning('普通用户无权访问用户管理页面');
+    return '/403';
+  }
 });
 
 export default router;

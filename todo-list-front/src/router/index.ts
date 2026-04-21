3fa68159f9e1d78535fa40c923eba8c48652225c
zaichuntian <3034983887@qq.com>
2026-04-22 06:31:14 +0800

    fix: 修改路由组件引入方式为动态路由、添加401、403、404页面路由规则并配置守卫规则

--- todo-list-front/src/router/index.ts
+++ todo-list-front/src/router/index.ts
@@ -1,29 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Login from '../views/Login.vue';
-import Home from '../views/Home.vue';
-import User from '../views/User.vue';
-import Todo from '../views/Todo.vue';
-import Layout from '../layouts/Layout.vue';
-import Category from '../views/Category.vue';
-import Page404 from '../views/404.vue';
-import Page403 from '../views/403.vue';
 import { ElMessage } from 'element-plus';
 import { UserInfo } from '@/types/user.ts';
 
 const routes = [
-  { path: '/login', component: Login },
+  { path: '/login', component: () => import('../views/Login.vue') },
   {
     path: '/',
-    component: Layout,
+    component: () => import('../layouts/Layout.vue'),
     children: [
-      { path: '', component: Home },
-      { path: '/user', component: User },
-      { path: '/todo', component: Todo },
-      { path: '/category', component: Category },
+      { path: '', component: () => import('../views/Home.vue') },
+      { path: '/user', component: () => import('../views/User.vue') },
+      { path: '/todo', component: () => import('../views/Todo.vue') },
+      { path: '/category', component: () => import('../views/Category.vue') },
     ],
   },
-  { path: '/403', component: Page403 },
-  { path: '/:pathMatch(.*)*', component: Page404 },
+  { path: '/401', component: () => import('../views/401.vue') },
+  { path: '/403', component: () => import('../views/403.vue') },
+  { path: '/:pathMatch(.*)*', component: () => import('../views/404.vue') },
 ];
 
 const router = createRouter({
@@ -33,8 +26,11 @@ const router = createRouter({
 
 router.beforeEach(to => {
   const userInfo = JSON.parse(localStorage.getItem('userInfo') || '{}') as UserInfo;
-  // 如果用户未登录，且尝试访问需要登录的路由，重定向到登录页
-  if (to.path !== '/login' && !userInfo.token && !userInfo.userUuid) return '/login';
+  // 如果用户未登录，且尝试访问需要登录的路由，重定向到401页面
+  if (to.path !== '/login' && to.path !== '/401' && !userInfo.token && !userInfo.userUuid) {
+    ElMessage.warning('请先登录');
+    return '/401';
+  }
   // 如果用户已登录，且尝试访问登录页，重定向到首页
   if (to.path === '/login' && userInfo.token && userInfo.userUuid) return '/';
   // 权限检查：只有超级管理员和管理员可以访问用户管理页面

a3be36e40a6e5d37a13be9fcaecf3061998ccaac
zaichuntian <3034983887@qq.com>
2026-04-23 07:16:12 +0800

    feat: 添加任务管理模块请求验证的中间件

--- todo-list-backend/src/routes/todo.route.ts
+++ todo-list-backend/src/routes/todo.route.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { TodoController } from '../controllers/todo.controller';
 import { authMiddleware } from '../middleware/auth.middleware';
+import { todoValidationRules, validate } from '../middleware/validation.middleware';
 
 const router = Router();
 
 router.get('/todos', authMiddleware, TodoController.getMyList);
-router.post('/todos', authMiddleware, TodoController.create);
-router.put('/todos/:uuid', authMiddleware, TodoController.update);
+router.post('/todos', authMiddleware, todoValidationRules.create, validate, TodoController.create);
+router.put('/todos/:uuid', authMiddleware, todoValidationRules.update, validate, TodoController.update);
 router.delete('/todos/:uuid', authMiddleware, TodoController.delete);
 router.put('/todos/:uuid/status', authMiddleware, TodoController.updateStatus);
 

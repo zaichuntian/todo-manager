9bd20b2887f3379a81d12a41e14532b97d43d7c7
zaichuntian <3034983887@qq.com>
2026-04-23 07:26:03 +0800

    feat: 对用户管理hook进行可选权限验证

--- todo-list-front/src/hooks/useTodo.ts
+++ todo-list-front/src/hooks/useTodo.ts
@@ -1,20 +1,14 @@
-import { ref, onMounted } from 'vue';
 import { getTodosApi, addTodoApi, updateTodoApi, deleteTodoApi, updateTodoStatusApi } from '../api/todo';
 import { useCrud } from './useCrud';
 import { formatTime } from '../utils/format';
+import { useAuth } from './useAuth';
 import type { Todo, TodoFormData } from '../types/todo';
-import type { UserInfo } from '../types/user';
 
 /**
  * 任务管理自定义 Hook
  */
 export function useTodo() {
-  const loginUser = ref<UserInfo>({
-    userUuid: '',
-    username: '',
-    role: 0,
-    token: '',
-  });
+  const { loginUser, hasPermission } = useAuth();
 
   // 使用通用 CRUD Hook
   const crud = useCrud<Todo, TodoFormData>({
@@ -32,33 +26,23 @@ export function useTodo() {
         { required: true, message: '请输入内容', trigger: 'blur' },
         { min: 2, max: 200, message: '2-200字符', trigger: 'blur' },
       ],
+      categoryUuid: [{ required: true, message: '请选择分类', trigger: 'change' }],
     },
     initialForm: {
       uuid: '',
       title: '',
       content: '',
+      categoryUuid: '',
     },
   });
 
   // 判断是否是自己的任务或有权限操作
   const isMyTask = (row: Todo) => {
-    // 超级管理员(2)和管理员(1)可以操作所有任务
-    if (loginUser.value.role === 2 || loginUser.value.role === 1) {
-      return true;
-    }
-    // 普通用户只能操作自己的任务
-    return row.userUuid === loginUser.value.userUuid;
+    return hasPermission(row.userUuid);
   };
 
   // 初始化数据
-  onMounted(() => {
-    // 从本地获取当前用户信息
-    const userInfoStr = localStorage.getItem('userInfo');
-    if (userInfoStr) {
-      loginUser.value = JSON.parse(userInfoStr);
-    }
-    crud.getList();
-  });
+  crud.getList();
 
   return {
     // 从 CRUD Hook 中解构出需要的状态和方法

4e1ac5615b690bdd90c3be497774d16da71f6935
zaichuntian <3034983887@qq.com>
2026-04-23 07:17:19 +0800

    feat: 添加任务相关类型定义

---
+++ todo-list-backend/src/types/todo.ts
@@ -0,0 +1,27 @@
+// 任务相关类型定义
+import type { PaginationParams, PaginationResponse, FormRules, UserBasic } from './common';
+
+export interface Todo {
+  uuid: string;
+  title: string;
+  content: string;
+  status: number;
+  userUuid: string;
+  categoryUuid: string;
+  user?: UserBasic;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface TodoFormData {
+  uuid: string;
+  title: string;
+  content: string;
+  categoryUuid: string;
+}
+
+export type TodoListParams = PaginationParams;
+
+export type TodoListResponse = PaginationResponse<Todo>;
+
+export type TodoRules = FormRules;

50ea7e8b1315a03fe65ec7e1b2137fb13f3f3b03
zaichuntian <3034983887@qq.com>
2026-04-23 07:15:28 +0800

    feat: 对分类信息进行权限验证

--- todo-list-backend/src/controllers/todo.controller.ts
+++ todo-list-backend/src/controllers/todo.controller.ts
@@ -3,14 +3,16 @@ import { TodoService } from '../services/todo.service';
 import { success, fail } from '../utils/response';
 import { BaseController } from './base.controller';
 import { CONSTANTS } from '../config/constants';
+import { CategoryService } from '../services/category.service';
 
 export class TodoController extends BaseController {
   static async getMyList(req: Request, res: Response) {
     try {
       const pageNum = Number(req.query.pageNum) || CONSTANTS.DEFAULT_PAGE_NUM;
       const pageSize = Number(req.query.pageSize) || CONSTANTS.DEFAULT_PAGE_SIZE;
+      const lastId = Number(req.query.lastId) || 0;
 
-      const data = await TodoService.findAllTodos(pageNum, pageSize);
+      const data = await TodoService.findAllTodos(pageNum, pageSize, lastId);
 
       // 修复：使用 BaseController.handlePaginationResponse
       BaseController.handlePaginationResponse(res, data, pageNum, pageSize);
@@ -22,20 +24,31 @@ export class TodoController extends BaseController {
 
   static async create(req: Request, res: Response) {
     try {
-      const { title, content } = req.body;
+      const { title, content, categoryUuid } = req.body;
       const userUuid = req.user!.uuid;
-
-      if (!title || !content) {
-        return res.json(fail('标题和内容不能为空'));
+      // 验证分类是否存在
+      const category = await CategoryService.findByUuid(categoryUuid);
+      if (!category) {
+        return res.json(fail('分类不存在'));
       }
 
-      await TodoService.create({
+      // 验证分类是否属于当前用户
+      if (category.userUuid !== userUuid) {
+        return res.json(fail('无权限使用该分类'));
+      }
+
+      if (!title || !content || !categoryUuid) {
+        return res.json(fail('标题、内容和分类不能为空'));
+      }
+
+      const todo = await TodoService.create({
         userUuid,
         title,
         content,
+        categoryUuid,
       });
 
-      return res.json(success(null, '创建成功'));
+      return res.json(success(todo, '创建成功'));
     } catch (err) {
       // 修复：使用 BaseController.handleError
       BaseController.handleError(res, err, '创建失败');

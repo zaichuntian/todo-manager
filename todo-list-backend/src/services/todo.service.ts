0d47dfb5ba1f47cfb7dd07df6886f50fbee42fc2
zaichuntian <3034983887@qq.com>
2026-04-23 07:17:01 +0800

    feat: 使用redis对任务数据进行数据缓存处理

--- todo-list-backend/src/services/todo.service.ts
+++ todo-list-backend/src/services/todo.service.ts
@@ -1,12 +1,23 @@
 import Todo from '../models/todo.model';
 import User from '../models/user.model';
-import { BaseService } from './base.service';
 import { Op } from 'sequelize';
+import { BaseService } from './base.service';
+import { RedisUtil } from '../utils/redis';
 
 export class TodoService extends BaseService<any> {
   // 获取所有任务（带分页）
-  static async findAllTodos(pageSize: number, lastId: number = 0) {
-    return await Todo.findAndCountAll({
+  static async findAllTodos(pageNum: number, pageSize: number, lastId: number = 0) {
+    // 生成缓存键
+    const cacheKey = `todos:all:${pageNum}:${pageSize}:${lastId}`;
+
+    // 尝试从缓存获取
+    const cachedData = await RedisUtil.get(cacheKey);
+    if (cachedData) {
+      return cachedData;
+    }
+
+    // 缓存未命中，从数据库查询
+    const data = await Todo.findAndCountAll({
       where: {
         isDeleted: 1,
         id: { [Op.gt]: lastId }, // 使用ID作为游标
@@ -21,6 +32,10 @@ export class TodoService extends BaseService<any> {
         },
       ],
     });
+
+    // 将结果存入缓存（过期时间 5 分钟）
+    await RedisUtil.set(cacheKey, data, 300);
+    return data;
   }
 
   // 根据 uuid 查询单个任务
@@ -30,16 +45,25 @@ export class TodoService extends BaseService<any> {
 
   // 创建任务
   static async create(todo: any) {
-    return await super.create(Todo, todo);
+    const result = await super.create(Todo, todo);
+    // 清除相关缓存
+    await RedisUtil.delByPattern('todos:all:*');
+    return result;
   }
 
   // 根据 uuid 修改任务
   static async updateByUuid(uuid: string, data: any) {
-    return await super.updateByUuid(Todo, uuid, data);
+    const result = await super.updateByUuid(Todo, uuid, data);
+    // 清除相关缓存
+    await RedisUtil.delByPattern('todos:all:*');
+    return result;
   }
 
   // 根据 uuid 软删除任务
   static async deleteByUuid(uuid: string) {
-    return await super.deleteByUuid(Todo, uuid);
+    const result = await super.deleteByUuid(Todo, uuid);
+    // 清除相关缓存
+    await RedisUtil.delByPattern('todos:all:*');
+    return result;
   }
 }

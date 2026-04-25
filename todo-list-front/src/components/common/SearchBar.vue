62f5463db097c8be8537abea9f65ac833f8e407f
zaichuntian <3034983887@qq.com>
2026-04-26 02:21:19 +0800

    feat: 创建搜索栏组件

---
+++ todo-list-front/src/components/common/SearchBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="search-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped lang="less">
+.search-bar {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+</style>
